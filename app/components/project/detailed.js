f774b551ee768629cd3b4aaf3958d647e3793e0e
Venus Ang <venusang24@gmail.com>
2020-02-03 13:00:28 -0800

    removed loader and tinkering with slider that shows-up on the project detail page

---
+++ app/components/project/detailed.js
@@ -0,0 +1,14 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { inject as service } from "@ember/service";
+import { task } from "ember-concurrency";
+
+export default class ProjectDetailedComponent extends Component {
+  @service store;
+  @tracked projectModel;
+  @task(function*() {
+    let projects = yield this.store.findAll("project");
+    this.projectModel = projects;
+  })
+  projects;
+}
