71c12e3c86d7eaa486bf24b202d339b88e6409c8
Venus Ang <venusang24@gmail.com>
2020-02-03 18:06:17 -0800

    updating detail template for smaller images

--- app/models/project.js
+++ app/models/project.js
@@ -3,6 +3,8 @@ import Model, { attr } from "@ember-data/model";
 export default class ProjectModel extends Model {
   @attr img1920x1080;
   @attr img426x574;
+  @attr img426x574x01;
+  @attr img426x574x02;
   @attr category;
   @attr title;
   @attr description;
