b6951c777e9c30668b43b63d7059c7aa34f8ba21
Venus Ang <venusang24@gmail.com>
2020-02-29 12:34:34 -0800

    Updated homepage projects images to updated json, addressed accessibility attributes for links

---
+++ app/components/request.js
@@ -0,0 +1,6 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+
+export default class RequestComponent extends Component {
+  @tracked companyEmail = "gblandscapes@yahoo.com";
+}
